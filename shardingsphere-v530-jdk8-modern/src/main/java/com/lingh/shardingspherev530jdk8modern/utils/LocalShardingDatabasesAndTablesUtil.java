50dcc4f4a039fd307474981f8f3de06e3af9b06d
linghengqian <linghengqian@outlook.com>
2023-04-07 18:05:07 +0800

    Fix the problem that JDBC Connection fails collectively after updating metadata

--- shardingsphere-v530-jdk8-modern/src/main/java/com/lingh/shardingspherev530jdk8modern/utils/LocalShardingDatabasesAndTablesUtil.java
+++ shardingsphere-v530-jdk8-modern/src/main/java/com/lingh/shardingspherev530jdk8modern/utils/LocalShardingDatabasesAndTablesUtil.java
@@ -1,18 +1,28 @@
 package com.lingh.shardingspherev530jdk8modern.utils;
 
+import org.apache.shardingsphere.distsql.parser.segment.AlgorithmSegment;
 import org.apache.shardingsphere.driver.jdbc.core.connection.ShardingSphereConnection;
-import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.rule.ShardingAutoTableRuleConfiguration;
 import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.strategy.sharding.StandardShardingStrategyConfiguration;
+import org.apache.shardingsphere.sharding.distsql.handler.update.AlterShardingTableRuleStatementUpdater;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.strategy.KeyGenerateStrategySegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.strategy.ShardingStrategySegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.table.AutoTableRuleSegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.table.TableRuleSegment;
+import org.apache.shardingsphere.sharding.distsql.parser.statement.AlterShardingTableRuleStatement;
 import org.apache.shardingsphere.sharding.rule.ShardingRule;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LocalShardingDatabasesAndTablesUtil {
@@ -36,36 +46,72 @@ public class LocalShardingDatabasesAndTablesUtil {
 
     public static void updateActualDataNodesByJupiter(DataSource dataSource, String databaseName, String logicTableName, String newActualDataNodes) {
         ContextManager contextManager = getContextManager(dataSource);
-        ShardingSphereRuleMetaData ruleMetaData = contextManager.getMetaDataContexts()
+        ShardingSphereDatabase database = contextManager.getMetaDataContexts()
                 .getMetaData()
                 .getDatabases()
-                .get(databaseName)
-                .getRuleMetaData();
+                .get(databaseName);
+        ShardingSphereRuleMetaData ruleMetaData = database.getRuleMetaData();
         Optional<ShardingRule> singleRule = ruleMetaData.findSingleRule(ShardingRule.class);
         assert singleRule.isPresent();
         ShardingRuleConfiguration currentRuleConfig = (ShardingRuleConfiguration) singleRule.get().getConfiguration();
-        Collection<ShardingTableRuleConfiguration> toBeAlteredTableRuleConfigList = new LinkedList<>();
-        currentRuleConfig.getTables().forEach(oldTableRuleConfig -> {
-            if (logicTableName.equals(oldTableRuleConfig.getLogicTable())) {
-                ShardingTableRuleConfiguration newTableRuleConfig = new ShardingTableRuleConfiguration(logicTableName, newActualDataNodes);
-                newTableRuleConfig.setDatabaseShardingStrategy(oldTableRuleConfig.getDatabaseShardingStrategy());
-                newTableRuleConfig.setTableShardingStrategy(oldTableRuleConfig.getTableShardingStrategy());
-                newTableRuleConfig.setKeyGenerateStrategy(oldTableRuleConfig.getKeyGenerateStrategy());
-                newTableRuleConfig.setAuditStrategy(oldTableRuleConfig.getAuditStrategy());
-                toBeAlteredTableRuleConfigList.add(newTableRuleConfig);
-            } else {
-                toBeAlteredTableRuleConfigList.add(oldTableRuleConfig);
-            }
-        });
-        currentRuleConfig.setTables(toBeAlteredTableRuleConfigList);
-        Collection<RuleConfiguration> toBeAlteredRuleConfigList = new LinkedList<>();
-        toBeAlteredRuleConfigList.add(currentRuleConfig);
-        ruleMetaData.getRules().stream().filter(shardingSphereRule -> !(shardingSphereRule instanceof ShardingRule))
-                .forEach(shardingSphereRule -> toBeAlteredRuleConfigList.add(shardingSphereRule.getConfiguration()));
-        contextManager.alterRuleConfiguration(databaseName, toBeAlteredRuleConfigList);
-        contextManager.getMetaDataContexts().getPersistService().getDatabaseRulePersistService().persist(databaseName, toBeAlteredRuleConfigList);
-        contextManager.reloadDatabaseMetaData(databaseName);
+
+
+        AlterShardingTableRuleStatementUpdater updater = new AlterShardingTableRuleStatementUpdater();
+        AlterShardingTableRuleStatement statement = new AlterShardingTableRuleStatement(Arrays.asList(
+                createCompleteAutoTableRule("t_order_item"), createCompleteTableRule("t_order")
+        ));
+        updater.checkSQLStatement(database, statement, currentRuleConfig);
+        ShardingRuleConfiguration toBeAlteredRuleConfig = updater.buildToBeAlteredRuleConfiguration(statement);
+        updater.updateCurrentRuleConfiguration(currentRuleConfig, toBeAlteredRuleConfig);
+        assert currentRuleConfig.getTables().size() == 1;
+
+        ShardingTableRuleConfiguration tableRule = currentRuleConfig.getTables().iterator().next();
+        assert tableRule.getLogicTable().equals("t_order_item");
+        assert tableRule.getActualDataNodes().equals("ds_${0..1}.t_order${0..1}");
+        assert tableRule.getTableShardingStrategy() instanceof StandardShardingStrategyConfiguration;
+        assert ((StandardShardingStrategyConfiguration) tableRule.getTableShardingStrategy()).getShardingColumn().equals("product_id");
+        assert tableRule.getTableShardingStrategy().getShardingAlgorithmName().equals("t_order_item_table_core.standard.fixture");
+        assert tableRule.getDatabaseShardingStrategy() instanceof StandardShardingStrategyConfiguration;
+        assert tableRule.getDatabaseShardingStrategy().getShardingAlgorithmName().equals("t_order_item_database_inline");
+        assert currentRuleConfig.getTables().size() == 1;
+
+        ShardingAutoTableRuleConfiguration autoTableRule = currentRuleConfig.getAutoTables().iterator().next();
+        assert autoTableRule.getLogicTable().equals("t_order");
+        assert autoTableRule.getActualDataSources().equals("ds_0,ds_1");
+        assert autoTableRule.getShardingStrategy().getShardingAlgorithmName().equals("t_order_foo.distsql.fixture");
+        assert ((StandardShardingStrategyConfiguration) autoTableRule.getShardingStrategy()).getShardingColumn().equals("order_id");
+        assert autoTableRule.getKeyGenerateStrategy().getColumn().equals("product_id");
+        assert autoTableRule.getKeyGenerateStrategy().getKeyGeneratorName().equals("t_order_distsql.fixture");
     }
+
+    private static AutoTableRuleSegment createCompleteAutoTableRule(final String logicTableName) {
+        AutoTableRuleSegment result = new AutoTableRuleSegment(logicTableName, Arrays.asList("ds_0", "ds_1"));
+        result.setKeyGenerateStrategySegment(
+                new KeyGenerateStrategySegment("product_id",
+                new AlgorithmSegment("DISTSQL.FIXTURE", new Properties()))
+        );
+        result.setShardingColumn("order_id");
+        result.setShardingAlgorithmSegment(new AlgorithmSegment("FOO.DISTSQL.FIXTURE", PropertiesBuilder.build(new PropertiesBuilder.Property("", ""))));
+        return result;
+    }
+
+    private static TableRuleSegment createCompleteTableRule(final String logicTableName) {
+        TableRuleSegment result = new TableRuleSegment(
+                logicTableName,
+                Collections.singletonList("ds_${0..1}.t_order${0..1}"),
+                new KeyGenerateStrategySegment("product_id", new AlgorithmSegment("DISTSQL.FIXTURE", new Properties())),
+                null
+        );
+        result.setTableStrategySegment(new ShardingStrategySegment("standard",
+                "product_id", new AlgorithmSegment("CORE.STANDARD.FIXTURE", new Properties())));
+        result.setDatabaseStrategySegment(
+                new ShardingStrategySegment("standard", "product_id",
+                        new AlgorithmSegment("inline",
+                                PropertiesBuilder.build(new PropertiesBuilder.Property("algorithm-expression", "ds_${user_id % 2}"))))
+        );
+        return result;
+    }
+
 
     private static ContextManager getContextManager(final DataSource dataSource) {
         try (ShardingSphereConnection connection = dataSource.getConnection().unwrap(ShardingSphereConnection.class)) {
--- shardingsphere-v530-jdk8-modern/src/test/java/com/lingh/shardingspherev530jdk8modern/ContextManagerTests.java
+++ shardingsphere-v530-jdk8-modern/src/test/java/com/lingh/shardingspherev530jdk8modern/ContextManagerTests.java
@@ -2,12 +2,24 @@ package com.lingh.shardingspherev530jdk8modern;
 
 import com.lingh.shardingspherev530jdk8modern.mapper.TOrderShardingSphereMapper;
 import com.lingh.shardingspherev530jdk8modern.utils.LocalShardingDatabasesAndTablesUtil;
+import com.lingh.shardingspherev530jdk8modern.utils.PropertiesBuilder;
+import org.apache.shardingsphere.distsql.parser.segment.AlgorithmSegment;
 import org.apache.shardingsphere.driver.jdbc.core.connection.ShardingSphereConnection;
 import org.apache.shardingsphere.infra.config.algorithm.AlgorithmConfiguration;
 import org.apache.shardingsphere.infra.config.rule.RuleConfiguration;
+import org.apache.shardingsphere.infra.metadata.database.ShardingSphereDatabase;
 import org.apache.shardingsphere.infra.metadata.database.rule.ShardingSphereRuleMetaData;
 import org.apache.shardingsphere.mode.manager.ContextManager;
 import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.rule.ShardingAutoTableRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
+import org.apache.shardingsphere.sharding.api.config.strategy.sharding.StandardShardingStrategyConfiguration;
+import org.apache.shardingsphere.sharding.distsql.handler.update.AlterShardingTableRuleStatementUpdater;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.strategy.KeyGenerateStrategySegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.strategy.ShardingStrategySegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.table.AutoTableRuleSegment;
+import org.apache.shardingsphere.sharding.distsql.parser.segment.table.TableRuleSegment;
+import org.apache.shardingsphere.sharding.distsql.parser.statement.AlterShardingTableRuleStatement;
 import org.apache.shardingsphere.sharding.rule.ShardingRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,13 +28,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Optional;
-import java.util.Properties;
+import java.util.*;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -41,6 +55,24 @@ public class ContextManagerTests {
         LocalShardingDatabasesAndTablesUtil.updateActualDataNodesByJupiter(
                 shardingSphereDataSource, oldDatabaseName, oldLogicTableName, newActualDataNodes
         );
+    }
+
+    @SuppressWarnings({"SqlDialectInspection", "SqlNoDataSourceInspection"})
+    @Test
+    void testMultipleUpdates() throws SQLException {
+        IntStream.range(0, 20).forEach(i -> {
+            String newActualDataNodes = "ds-0.t_order_$->{20221010..20221012}";
+            String oldLogicTableName = "t_order_sharding_sphere";
+            String oldDatabaseName = "sharding_db";
+            LocalShardingDatabasesAndTablesUtil.updateActualDataNodesByJupiter(
+                    shardingSphereDataSource, oldDatabaseName, oldLogicTableName, newActualDataNodes
+            );
+        });
+        try (Connection connection = shardingSphereDataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("select * from t_order_sharding_sphere")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            assertThat(resultSet).isNotNull();
+        }
     }
 
     @Test
@@ -111,5 +143,59 @@ public class ContextManagerTests {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    void testTemp(){
+        ContextManager contextManager = getContextManager(shardingSphereDataSource);
+        ShardingSphereDatabase database = contextManager.getMetaDataContexts()
+                .getMetaData()
+                .getDatabases()
+                .get("sharding_db");
+        ShardingSphereRuleMetaData ruleMetaData = database.getRuleMetaData();
+        Optional<ShardingRule> singleRule = ruleMetaData.findSingleRule(ShardingRule.class);
+        assert singleRule.isPresent();
+        ShardingRuleConfiguration currentRuleConfig = (ShardingRuleConfiguration) singleRule.get().getConfiguration();
+        AlterShardingTableRuleStatementUpdater updater = new AlterShardingTableRuleStatementUpdater();
+        AlterShardingTableRuleStatement statement = new AlterShardingTableRuleStatement(Arrays.asList(
+                createCompleteAutoTableRule("t_order_item"), createCompleteTableRule("t_order")
+        ));
+        updater.checkSQLStatement(database, statement, currentRuleConfig);
+        ShardingRuleConfiguration toBeAlteredRuleConfig = updater.buildToBeAlteredRuleConfiguration(statement);
+        updater.updateCurrentRuleConfiguration(currentRuleConfig, toBeAlteredRuleConfig);
+        assert currentRuleConfig.getTables().size() == 1;
+        ShardingTableRuleConfiguration tableRule = currentRuleConfig.getTables().iterator().next();
+        assert tableRule.getLogicTable().equals("t_order_item");
+        assert tableRule.getActualDataNodes().equals("ds_${0..1}.t_order${0..1}");
+        assert tableRule.getTableShardingStrategy() instanceof StandardShardingStrategyConfiguration;
+        assert ((StandardShardingStrategyConfiguration) tableRule.getTableShardingStrategy()).getShardingColumn().equals("product_id");
+        assert tableRule.getTableShardingStrategy().getShardingAlgorithmName().equals("t_order_item_table_core.standard.fixture");
+        assert tableRule.getDatabaseShardingStrategy() instanceof StandardShardingStrategyConfiguration;
+        assert tableRule.getDatabaseShardingStrategy().getShardingAlgorithmName().equals("t_order_item_database_inline");
+        assert currentRuleConfig.getTables().size() == 1;
+        ShardingAutoTableRuleConfiguration autoTableRule = currentRuleConfig.getAutoTables().iterator().next();
+        assert autoTableRule.getLogicTable().equals("t_order");
+        assert autoTableRule.getActualDataSources().equals("ds_0,ds_1");
+        assert autoTableRule.getShardingStrategy().getShardingAlgorithmName().equals("t_order_foo.distsql.fixture");
+        assert ((StandardShardingStrategyConfiguration) autoTableRule.getShardingStrategy()).getShardingColumn().equals("order_id");
+        assert autoTableRule.getKeyGenerateStrategy().getColumn().equals("product_id");
+        assert autoTableRule.getKeyGenerateStrategy().getKeyGeneratorName().equals("t_order_distsql.fixture");
+    }
+
+    private static AutoTableRuleSegment createCompleteAutoTableRule(final String logicTableName) {
+        AutoTableRuleSegment result = new AutoTableRuleSegment(logicTableName, Arrays.asList("ds_0", "ds_1"));
+        result.setKeyGenerateStrategySegment(new KeyGenerateStrategySegment("product_id", new AlgorithmSegment("DISTSQL.FIXTURE", new Properties())));
+        result.setShardingColumn("order_id");
+        result.setShardingAlgorithmSegment(new AlgorithmSegment("FOO.DISTSQL.FIXTURE", PropertiesBuilder.build(new PropertiesBuilder.Property("", ""))));
+        return result;
+    }
+
+    private static TableRuleSegment createCompleteTableRule(final String logicTableName) {
+        KeyGenerateStrategySegment keyGenerator = new KeyGenerateStrategySegment("product_id", new AlgorithmSegment("DISTSQL.FIXTURE", new Properties()));
+        TableRuleSegment result = new TableRuleSegment(logicTableName, Collections.singletonList("ds_${0..1}.t_order${0..1}"), keyGenerator, null);
+        result.setTableStrategySegment(new ShardingStrategySegment("standard", "product_id", new AlgorithmSegment("CORE.STANDARD.FIXTURE", new Properties())));
+        AlgorithmSegment databaseAlgorithmSegment = new AlgorithmSegment("inline", PropertiesBuilder.build(new PropertiesBuilder.Property("algorithm-expression", "ds_${user_id % 2}")));
+        result.setDatabaseStrategySegment(new ShardingStrategySegment("standard", "product_id", databaseAlgorithmSegment));
+        return result;
     }
 }
